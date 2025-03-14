4e14e4383c7335aa72d486b66bc0ac0258b7ace7
meeeeeooooowwwwwww <generalflynn17@gmail.com>
2025-03-15 01:55:33 +1300

    feat: add Google Analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -64,6 +64,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RPYVDP2KGZ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RPYVDP2KGZ');
+            `
+          }}
+        />
+      </head>
       <body className="overflow-hidden m-0 p-0">{children}</body>
     </html>
   )
